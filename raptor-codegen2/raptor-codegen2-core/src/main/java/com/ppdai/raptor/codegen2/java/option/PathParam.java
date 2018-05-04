dc772fa316e5d1fed8ef150220d85b0ac495f159
zhangchengxi <orezsilence@163.com>
2018-05-04 11:25:37 +0800

    回退JavaGenerator,使生成的Message类改动尽量小

--- raptor-codegen2/raptor-codegen2-core/src/main/java/com/ppdai/raptor/codegen2/java/option/PathParam.java
+++ raptor-codegen2/raptor-codegen2-core/src/main/java/com/ppdai/raptor/codegen2/java/option/PathParam.java
@@ -26,25 +26,24 @@ public class PathParam {
     private static final ProtoMember REQUEST_MAPPING_NAME = ProtoMember.get(PATH_PARAM_TYPE, "name");
     private static final ProtoMember REQUEST_MAPPING_TYPE = ProtoMember.get(PATH_PARAM_TYPE, "type");
     private static final Map<String, TypeName> BUILD_IN_TYPE_MAP =
-            // TODO: 2018/5/3 补充类型
             ImmutableMap.<String, TypeName>builder()
-                    .put("TYPE_DOUBLE", TypeName.DOUBLE)
-                    .put("TYPE_FLOAT", TypeName.FLOAT)
-                    .put("TYPE_INT64", TypeName.LONG)
-                    .put("TYPE_UINT64", TypeName.LONG)
-                    .put("TYPE_INT32", TypeName.INT)
-                    .put("TYPE_FIXED64", TypeName.LONG)
-                    .put("TYPE_FIXED32", TypeName.INT)
-                    .put("TYPE_BOOL", TypeName.BOOLEAN)
-                    .put("TYPE_STRING", ClassName.get(String.class))
-                    .put("TYPE_BYTES", ClassName.get(ByteString.class))
-                    .put("TYPE_UINT32", TypeName.INT)
-                    .put("TYPE_SFIXED32", TypeName.INT)
-                    .put("TYPE_SFIXED64", TypeName.LONG)
-                    .put("TYPE_SINT32", TypeName.INT)
-                    .put("TYPE_SINT64", TypeName.LONG)
+                    .put("double", TypeName.DOUBLE)
+                    .put("float", TypeName.FLOAT)
+                    .put("int64", TypeName.LONG)
+                    .put("uint64", TypeName.LONG)
+                    .put("int32", TypeName.INT)
+                    .put("fixed64", TypeName.LONG)
+                    .put("fixed32", TypeName.INT)
+                    .put("bool", TypeName.BOOLEAN)
+                    .put("string", ClassName.get(String.class))
+                    .put("bytes", ClassName.get(ByteString.class))
+                    .put("uint32", TypeName.INT)
+                    .put("sfixed32", TypeName.INT)
+                    .put("sfixed64", TypeName.LONG)
+                    .put("sint32", TypeName.INT)
+                    .put("sint64", TypeName.LONG)
                     .put("int",TypeName.INT)
-                    .put("string",ClassName.get(String.class))
+                    .put("boolean",TypeName.BOOLEAN)
                     .build();
 
     private String name;
@@ -56,13 +55,7 @@ public class PathParam {
         return new PathParam(name, type);
     }
 
-    public static TypeName getJavaType(String type) {
-        TypeName typeName = BUILD_IN_TYPE_MAP.get(type);
-        if(Objects.isNull(typeName)){
-            throw new RuntimeException("type name not supported: type="+type);
-        }
-        return typeName;
-    }
+
 
 
 }

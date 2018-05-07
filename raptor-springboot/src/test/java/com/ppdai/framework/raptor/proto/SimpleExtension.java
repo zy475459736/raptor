bab951cd4453f3e8088e96d3cd193e8fddb2b45f
yinzuolong <yinzuolong@ppdai.com>
2018-05-07 19:42:02 +0800

    增加RequestParam参数定义

--- raptor-springboot/src/test/java/com/ppdai/framework/raptor/proto/SimpleExtension.java
+++ raptor-springboot/src/test/java/com/ppdai/framework/raptor/proto/SimpleExtension.java
@@ -4,6 +4,7 @@ import com.ppdai.framework.raptor.annotation.RaptorInterface;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author yinzuolong
@@ -13,19 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface SimpleExtension {
 
     @RequestMapping("/get1")
-    AllTypesPojo testGet1(AllTypesPojo request);
+    AllTypesPojo testGet1(AllTypesPojo request, @RequestParam("p1") String p1);
 
     @RequestMapping("/get2/{p1}")
     AllTypesPojo testGet2(AllTypesPojo request, @PathVariable("p1") String p1);
 
     @RequestMapping(path = "/post1", method = RequestMethod.POST)
-    AllTypesPojo testPost1(AllTypesPojo request);
+    AllTypesPojo testPost1(AllTypesPojo request, @RequestParam("p1") String p1);
 
     @RequestMapping(path = "/post2/{p1}/{p2}", method = RequestMethod.POST)
     AllTypesPojo testPost2(AllTypesPojo request, @PathVariable("p1") String p1, @PathVariable("p2") int p2);
 
     @RequestMapping(path = "/put1", method = RequestMethod.PUT)
-    AllTypesPojo testPut1(AllTypesPojo request);
+    AllTypesPojo testPut1(AllTypesPojo request, @RequestParam("p1") String p1);
 
     @RequestMapping(path = "/delete/{p2}", method = RequestMethod.DELETE)
     AllTypesPojo testDelete1(AllTypesPojo request, @PathVariable("p2") int p2);

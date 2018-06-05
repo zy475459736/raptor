3dc8afbc9a72afcbd3e173e338587dee557ccfb4
yinzuolong <yinzuolong@ppdai.com>
2018-06-05 15:28:39 +0800

    增加测试类

---
+++ raptor-springboot/src/test/java/com/ppdai/framework/raptor/spring/TestApplication.java
@@ -0,0 +1,15 @@
+package com.ppdai.framework.raptor.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinzuolong
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}

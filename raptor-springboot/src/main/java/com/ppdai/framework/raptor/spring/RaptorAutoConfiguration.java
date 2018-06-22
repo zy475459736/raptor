d2d0f9ee9df4ce7ec97a2c16ca2ee011cf04091c
yinzuolong <yinzuolong@ppdai.com>
2018-06-22 17:37:42 +0800

    增加服务端异常处理

--- raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/RaptorAutoConfiguration.java
+++ raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/RaptorAutoConfiguration.java
@@ -1,11 +1,7 @@
 package com.ppdai.framework.raptor.spring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ppdai.framework.raptor.spring.converter.RaptorMessageConverter;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,19 +9,11 @@ import org.springframework.context.annotation.Configuration;
  * @author yinzuolong
  */
 @Configuration
-public class RaptorAutoConfiguration implements ApplicationContextAware {
-
-    private ApplicationContext applicationContext;
+public class RaptorAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
     public RaptorMessageConverter createRaptorMessageConverter() {
-        ObjectMapper objectMapper = this.applicationContext.getBean(ObjectMapper.class);
-        return objectMapper == null ? new RaptorMessageConverter() : new RaptorMessageConverter(objectMapper);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        return new RaptorMessageConverter();
     }
 }

6f6b3d6c4bc41392b1fb44fcaf4dd28f80716446
yinzuolong <yinzuolong@ppdai.com>
2018-06-25 19:58:24 +0800

    增加单元测试

--- raptor-springboot/src/test/java/com/ppdai/framework/raptor/spring/integration/RaptorIntegrationTest.java
+++ raptor-springboot/src/test/java/com/ppdai/framework/raptor/spring/integration/RaptorIntegrationTest.java
@@ -17,7 +17,7 @@ import org.springframework.util.SocketUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class IntegrationTestRaptor {
+public class RaptorIntegrationTest {
 
     @RaptorClient
     private Simple simple;
@@ -44,10 +44,42 @@ public class IntegrationTestRaptor {
     }
 
     @Test
-    public void testMoreService() {
+    public void getGet1() {
+        HelloRequest helloRequest = new HelloRequest();
+        helloRequest.setName("ppdai");
+        HelloReply reply = moreService.testGet1(helloRequest);
+        Assert.assertEquals("testGet1", reply.getMessage());
+    }
+
+    @Test
+    public void testGet2() {
+        HelloRequest helloRequest = new HelloRequest();
+        helloRequest.setName("ppdai");
+        HelloReply reply = moreService.testGet2(helloRequest);
+        Assert.assertEquals("testGet2", reply.getMessage());
+    }
+
+    @Test
+    public void testPost1() {
         HelloRequest helloRequest = new HelloRequest();
         helloRequest.setName("ppdai");
         HelloReply reply = moreService.testPost1(helloRequest);
-        Assert.assertEquals("ppdai", reply.getRequest().getName());
+        Assert.assertEquals("testPost1", reply.getMessage());
+    }
+
+    @Test
+    public void testPut1() {
+        HelloRequest helloRequest = new HelloRequest();
+        helloRequest.setName("ppdai");
+        HelloReply reply = moreService.testPut1(helloRequest);
+        Assert.assertEquals("testPut1", reply.getMessage());
+    }
+
+    @Test
+    public void testDelete1() {
+        HelloRequest helloRequest = new HelloRequest();
+        helloRequest.setName("ppdai");
+        HelloReply reply = moreService.testDelete1(helloRequest);
+        Assert.assertEquals("testDelete1", reply.getMessage());
     }
 }

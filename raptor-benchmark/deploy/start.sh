65eb20408fea20ac478447cfbb0675fce9e2576f
yinzuolong <yinzuolong@ppdai.com>
2018-06-26 16:38:28 +0800

    benchmark支持springboot，不能用benchmark插件，使用maven-dependency-plugin插件将所有依赖的jar放到lib下，运行start.sh脚本

---
+++ raptor-benchmark/deploy/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+APP_HOME=/opt/app/raptor
+
+APP_MAINCLASS=org.openjdk.jmh.Main
+
+CLASSPATH=$APP_HOME
+
+for i in "$APP_HOME"/lib/*.jar; do
+    CLASSPATH="$CLASSPATH":"$i"
+done
+
+JAVA_CMD="java -classpath $CLASSPATH $APP_MAINCLASS"
+
+echo $JAVA_CMD
+
+$JAVA_CMD

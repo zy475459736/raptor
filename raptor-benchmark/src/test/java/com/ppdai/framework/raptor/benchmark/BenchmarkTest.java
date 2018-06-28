ec2592be5beb8e5a86d41c30d986a8b94dec5e03
yinzuolong <yinzuolong@ppdai.com>
2018-06-28 23:09:55 +0800

    修改pom

---
+++ raptor-benchmark/src/test/java/com/ppdai/framework/raptor/benchmark/BenchmarkTest.java
@@ -0,0 +1,12 @@
+package com.ppdai.framework.raptor.benchmark;
+
+import org.junit.Test;
+
+/**
+ * @author yinzuolong
+ */
+public class BenchmarkTest {
+    @Test
+    public void name() {
+    }
+}

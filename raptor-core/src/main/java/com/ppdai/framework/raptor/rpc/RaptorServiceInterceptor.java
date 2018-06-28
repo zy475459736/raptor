ae45ee1f6c46b9ef16701369501480f8d0a5ffd9
yinzuolong <yinzuolong@ppdai.com>
2018-06-28 22:27:39 +0800

    增加注释

--- raptor-core/src/main/java/com/ppdai/framework/raptor/rpc/RaptorServiceInterceptor.java
+++ raptor-core/src/main/java/com/ppdai/framework/raptor/rpc/RaptorServiceInterceptor.java
@@ -5,9 +5,30 @@ package com.ppdai.framework.raptor.rpc;
  */
 public interface RaptorServiceInterceptor {
 
+    /**
+     * 调用服务方法之前处理
+     *
+     * @param request
+     * @param response
+     * @throws Exception
+     */
     void preHandle(RaptorRequest request, RaptorResponse response) throws Exception;
 
+    /**
+     * 调用服务方法成功后处理
+     *
+     * @param request
+     * @param response
+     * @throws Exception
+     */
     void postHandle(RaptorRequest request, RaptorResponse response) throws Exception;
 
+    /**
+     * 调用服务方法完成后处理，通过<code>response.getException()</code>获取异常
+     *
+     * @param request
+     * @param response
+     * @throws Exception
+     */
     void afterCompletion(RaptorRequest request, RaptorResponse response) throws Exception;
 }

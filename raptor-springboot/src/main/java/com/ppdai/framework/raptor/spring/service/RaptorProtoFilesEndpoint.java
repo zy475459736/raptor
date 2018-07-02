fd875305dffa53e746c15430f87ff526c55625e8
yinzuolong <yinzuolong@ppdai.com>
2018-07-02 20:19:10 +0800

    RaptorProtoFilesEndpoint完善

--- raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/service/RaptorProtoFilesEndpoint.java
+++ raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/service/RaptorProtoFilesEndpoint.java
@@ -4,14 +4,16 @@ import com.ppdai.framework.raptor.spring.utils.ProtoFileResourceUtils;
 import com.ppdai.framework.raptor.spring.utils.RaptorInterfaceUtils;
 import lombok.Setter;
 import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.core.MethodParameter;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author yinzuolong
@@ -31,17 +33,22 @@ public class RaptorProtoFilesEndpoint extends AbstractEndpoint {
     @Override
     public Object invoke() {
         Map<String, ProtoFileResourceUtils.ProtoFileInfo> protoFileInfoMap = new HashMap<>();
-        Set<Class<?>> raptorInterfaces = new HashSet<>();
         Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
         handlerMethods.forEach((requestMappingInfo, handlerMethod) -> {
             Class<?> raptorInterface = RaptorInterfaceUtils.getInterfaceClass(handlerMethod.getBeanType(), handlerMethod.getMethod());
-            if (raptorInterface != null && !raptorInterfaces.contains(raptorInterface)) {
-                raptorInterfaces.add(raptorInterface);
-                ProtoFileResourceUtils.ProtoFileInfo protoFileInfo = ProtoFileResourceUtils.findProtoFile(raptorInterface, classLoader);
+            if (raptorInterface != null) {
+                ProtoFileResourceUtils.ProtoFileInfo protoFileInfo = ProtoFileResourceUtils.findInterfaceProtoFile(raptorInterface, classLoader);
                 if (protoFileInfo != null) {
                     protoFileInfoMap.putIfAbsent(protoFileInfo.getFullName(), protoFileInfo);
                 }
             }
+            MethodParameter[] methodParameters = handlerMethod.getMethodParameters();
+            List<Class<?>> messageClasses = Arrays.stream(methodParameters).map(MethodParameter::getParameterType).collect(Collectors.toList());
+            MethodParameter methodReturnType = handlerMethod.getReturnType();
+            messageClasses.add(methodReturnType.getParameterType());
+
+            List<ProtoFileResourceUtils.ProtoFileInfo> messageProtoFiles = ProtoFileResourceUtils.findProtoFilesByMessage(messageClasses, classLoader);
+            messageProtoFiles.forEach(protoFileInfo -> protoFileInfoMap.putIfAbsent(protoFileInfo.getFullName(), protoFileInfo));
         });
         return protoFileInfoMap;
     }
--- raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/utils/ProtoFileResourceUtils.java
+++ raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/utils/ProtoFileResourceUtils.java
@@ -1,26 +1,28 @@
 package com.ppdai.framework.raptor.spring.utils;
 
 import com.ppdai.framework.raptor.annotation.RaptorInterface;
+import com.ppdai.framework.raptor.annotation.RaptorMessage;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ResolvableType;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.ConcurrentReferenceHashMap;
 import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author yinzuolong
  */
+@Slf4j
 public class ProtoFileResourceUtils {
 
     @Data
     public static class ProtoFileInfo {
-        private String appId;
-        private String appName;
         private String fileName;
         private String packageName;
         private String content;
@@ -30,37 +32,84 @@ public class ProtoFileResourceUtils {
         }
     }
 
-    private static Map<String, String> PROTO_FILE_CACHE = new ConcurrentReferenceHashMap<>();
+    private static Map<String, ProtoFileInfo> PROTO_FILE_CACHE = new ConcurrentReferenceHashMap<>();
 
-    public static ProtoFileInfo findProtoFile(Class<?> raptorInterface, ClassLoader classLoader) {
+    public static ProtoFileInfo findInterfaceProtoFile(Class<?> raptorInterface, ClassLoader classLoader) {
         if (raptorInterface != null) {
             RaptorInterface annotation = AnnotationUtils.findAnnotation(raptorInterface, RaptorInterface.class);
             if (annotation != null) {
-                ProtoFileInfo protoFileInfo = new ProtoFileInfo();
-                protoFileInfo.setAppId(annotation.appId());
-                protoFileInfo.setAppName(annotation.appName());
-                protoFileInfo.setFileName(StringUtils.endsWithIgnoreCase(annotation.protoFile(), ".proto") ? annotation.protoFile() : annotation.protoFile() + ".proto");
-                protoFileInfo.setPackageName(raptorInterface.getPackage().getName());
-                protoFileInfo.setContent(getProtoFileContent(protoFileInfo.packageName, protoFileInfo.fileName, classLoader));
-                return protoFileInfo;
+                return findProtoFile(raptorInterface.getPackage().getName(), annotation.protoFile(), classLoader);
             }
         }
         return null;
     }
 
-    public static String getProtoFileContent(String packageName, String fileName, ClassLoader classLoader) {
+    public static ProtoFileInfo findMessageProtoFile(Class<?> messageClass, ClassLoader classLoader) {
+        RaptorMessage annotation = AnnotationUtils.findAnnotation(messageClass, RaptorMessage.class);
+        if (annotation != null) {
+            return findProtoFile(messageClass.getPackage().getName(), annotation.protoFile(), classLoader);
+        }
+        return null;
+    }
+
+    public static List<ProtoFileInfo> findProtoFilesByMessage(List<Class<?>> messageClasses, ClassLoader classLoader) {
+        Set<ProtoFileInfo> results = new LinkedHashSet<>();
+        Set<Class<?>> founds = new HashSet<>();
+        for (Class<?> messageClass : messageClasses) {
+            Set<ProtoFileInfo> messageProtoFiles = findProtoFilesByMessage(messageClass, founds, classLoader);
+            results.addAll(messageProtoFiles);
+        }
+        return new ArrayList<>(results);
+    }
+
+    private static Set<ProtoFileInfo> findProtoFilesByMessage(Class<?> messageClass, Set<Class<?>> founds, ClassLoader classLoader) {
+        Set<ProtoFileInfo> results = new HashSet<>();
+        ProtoFileInfo protoFileInfo = findMessageProtoFile(messageClass, classLoader);
+        if (protoFileInfo != null && !founds.contains(messageClass)) {
+            results.add(protoFileInfo);
+            founds.add(messageClass);
+            Field[] fields = messageClass.getDeclaredFields();
+            for (Field field : fields) {
+                Class<?> fieldType = field.getType();
+                RaptorMessage annotation = AnnotationUtils.findAnnotation(fieldType, RaptorMessage.class);
+                ResolvableType resolvableType = ResolvableType.forField(field);
+                if (annotation != null) {
+                    Set<ProtoFileInfo> filedMessageProtoFiles = findProtoFilesByMessage(fieldType, founds, classLoader);
+                    results.addAll(filedMessageProtoFiles);
+                } else if (List.class.isAssignableFrom(fieldType)) {
+                    ResolvableType genericType = resolvableType.getGeneric(0);
+                    results.addAll(findProtoFilesByMessage(genericType.getRawClass(), founds, classLoader));
+                } else if (Map.class.isAssignableFrom(fieldType)) {
+                    ResolvableType keyType = resolvableType.getGeneric(0);
+                    ResolvableType valueType = resolvableType.getGeneric(1);
+                    results.addAll(findProtoFilesByMessage(keyType.getRawClass(), founds, classLoader));
+                    results.addAll(findProtoFilesByMessage(valueType.getRawClass(), founds, classLoader));
+                }
+            }
+        }
+        return results;
+    }
+
+    public static ProtoFileInfo findProtoFile(String packageName, String fileName, ClassLoader classLoader) {
+        fileName = StringUtils.endsWithIgnoreCase(fileName, ".proto") ? fileName : fileName + ".proto";
         String resource = packageName.replace(".", "/") + "/" + fileName;
         if (PROTO_FILE_CACHE.get(resource) != null) {
             return PROTO_FILE_CACHE.get(resource);
         }
+        ProtoFileInfo protoFileInfo = new ProtoFileInfo();
+        protoFileInfo.setFileName(fileName);
+        protoFileInfo.setPackageName(packageName);
+
         if (classLoader == null) {
             classLoader = ProtoFileResourceUtils.class.getClassLoader();
         }
         try (InputStream in = classLoader.getResourceAsStream(resource)) {
             String content = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
-            PROTO_FILE_CACHE.put(resource, content);
-            return content;
-        } catch (IOException ignored) {
+            protoFileInfo.setContent(content);
+            PROTO_FILE_CACHE.put(resource, protoFileInfo);
+            return protoFileInfo;
+        } catch (Exception e) {
+            log.error("Read proto file [{}] error.", resource, e);
         }
         return null;
     }
